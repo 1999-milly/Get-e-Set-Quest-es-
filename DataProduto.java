bf773faf7c2cfebd52a9cabfe24b9580b2a2fd24
1999-milly <wemilly.carv@unifesspa.edu.br>
2025-04-30 18:07:18 -0300

    Add files via upload

---
+++ DataProduto.java
@@ -0,0 +1,27 @@
+package eb;
+public class Data{
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data(int dia,int mes,int ano){
+        this.dia=dia;
+        this.mes=mes;
+        this.ano=ano;
+        if(!verificaData()){
+            System.out.println("Digite uma data valida");
+        }
+    }
+    public boolean verificaData(){
+        if(dia<=0 || dia>31) return false;
+        if(mes<=0 || mes>12) return false;
+        if(ano<=0 || ano>2025) return false;
+        return true;
+    }
+    public boolean isBefore(Data atual){
+        if(this.ano<atual.ano) return true;
+        if(this.ano==atual.ano && this.mes<atual.mes) return true;
+        if(this.ano==atual.ano && this.mes==atual.mes && this.dia<atual.dia) return true;
+        return false;
+    }
+}
