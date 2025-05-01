8fb65f5cb8ac634bf75948ae5b0a90de96ff99f6
1999-milly <wemilly.carv@unifesspa.edu.br>
2025-05-01 14:22:50 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -10,7 +10,7 @@ public class Conta{
         this.saldo=saldo;
         this.titular=titular;
         this.agencia=agencia;
-        System.out.println("Titular: "+titular+" | Numero: "+numero+" | Saldo: "+saldo+" | Agencia: "+agencia);
+        System.out.println("\nTitular: "+titular+" | Numero: "+numero+" | Saldo: "+saldo+" | Agencia: "+agencia);
     }
     public boolean saca(double valor){
         if(this.saldo>=valor){
